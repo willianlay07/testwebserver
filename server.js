0252bb2f171d1abe3f94c5fd166f6d4f5ddc3916
willianlay07 <willianlay07@gmail.com>
2016-12-26 00:23:47 +0800

    Initial Repo

---
+++ server.js
@@ -0,0 +1,33 @@
+var express 		= require('express');
+var app 			= express();
+var PORT 			= 3000;
+
+
+var middleware		= {
+		requireAuthentication: function(req, res, next){
+			console.log('Private route hit!');
+			next();
+		},
+		logger: function(req, res, next){
+			console.log('Requests '+ new Date().toString() +' '+req.method+' '+req.originalUrl);
+			next();
+		}
+};
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+app.use(express.static(__dirname+'/public'));
+
+// app.get('/', function(req, res){
+// 	res.send('Hello Express!');
+// });
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('About Us!');
+})
+
+
+app.listen(PORT, function(){
+	console.log('Server is running now.')
+});
